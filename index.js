ad3a29eafe7e175eecbea03d92a0a5ce465398db
Sascha Hillig <sascha@wonderweblabs.com>
2017-08-27 15:24:16 +0200

    Update coffee-script and haml-coffee dependencies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var through = require('through');
 var CoffeeScript = require('coffee-script');
-var Compiler = require('./node_modules/haml-coffee/src/haml-coffee')
+var Compiler = require('haml-coffee')
 
 module.exports = function(file) {
   if (!/\.haml|\.hamlc/.test(file)) return through();
@@ -13,14 +13,14 @@ module.exports = function(file) {
     source += chunk.toString();
   },
   function() {
-    
+
     var compiler = new Compiler({});
     compiler.parse(source);
     var template = CoffeeScript.compile(compiler.precompile(), {bare: true});
     var compiled = "module.exports = function(options) {\nreturn (function() {\n" +
       template +
       "\n}).call(options)\n};";
-    
+
     this.queue(compiled);
     this.queue(null);
   });
